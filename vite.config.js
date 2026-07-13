2698710ccfba02490e7c0de4bc0ab82cda953ec4
Wayne <a27689259@gmail.com>
2026-07-13 14:57:02 +0800

    BIM

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/3DBMI/',
+  base: '/3DBIM/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['web-ifc'],
